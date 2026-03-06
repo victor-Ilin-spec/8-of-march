df2f03454ec5cc0d148862aeb58e955e0c224b83
витюша <@DESKTOP-6OTE2G2>
2026-03-06 22:30:20 +0300

    Добавьте файлы проекта.

---
+++ main.js
@@ -0,0 +1,24 @@
+function en_lang() {
+    document.getElementById("zag").textContent = "that a site of congrutilations on 8 of march";
+    document.getElementById("nav").textContent = "navigation(links under is clickable)";
+    document.getElementById("tagosn").textContent = "main part of congrutilations";
+    document.getElementById("tagcons").textContent = "constructor of your congrutilations";
+    document.getElementById("osncon").textContent = "All girls, today is your beautiful day!";
+    document.getElementById("seccon").textContent = "I wish you happiness, health, and of course, a wish of love on this beautiful day! May you always be as charming and lovely as you are today!";
+    document.getElementById("consst").textContent = "#constructor is starting here";
+    document.getElementById("yt").textContent = "your text here";
+    document.getElementById("yth").textContent = "your text here";
+    document.getElementById("ythh").textContent = "your text here";
+}
+function ru_lang() {
+    document.getElementById("zag").textContent = "это сайт поздравление с 8 марта";
+    document.getElementById("nav").textContent = "навигация(ссылки ниже кликабельны)";
+    document.getElementById("tagosn").textContent = "основная часть поздравления";
+    document.getElementById("tagcons").textContent = "конструктор где можно создать своё поздравление";
+    document.getElementById("osncon").textContent = "все девушки, сегодня или уже прошёл ваш прекрасный день!";
+    document.getElementById("seccon").textContent = "желаю вам счастья, здоровья, и как же не обойтись без пожелания любви в этот прекрасный день! Что бы вы были всегда такими очаровашками и милашками!";
+    document.getElementById("consst").textContent = "#начало конструктора";
+    document.getElementById("yt").textContent = "ваш текст тут";
+    document.getElementById("yth").textContent = "ваш текст тут";
+    document.getElementById("ythh").textContent = "ваш текст тут";
+}
